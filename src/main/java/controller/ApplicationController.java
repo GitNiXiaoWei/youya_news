0e2e0ad06903a6a6ec64efc0bbb934b92215942c
Tinhozo-coder <409780062@qq.com>
2019-08-29 11:43:26 +0800

    熊峰8月29号

--- src/main/java/controller/ApplicationController.java
+++ src/main/java/controller/ApplicationController.java
@@ -1,10 +1,15 @@
 package controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import service.ApplyService;
+import vo.UserListVO;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("administrator")
@@ -13,11 +18,17 @@ public class ApplicationController {
     ApplyService applyService;
 
     /*
-    * 获取所有用户申请信息列表
+    * 获取所有用户申请信息列表以及分页
     * */
     @RequestMapping("listAllUserInfo")
-    public Object listAllUserInfo(){
-        return applyService.listAllUserInfo();
+    public Object listAllUserInfo(@RequestParam(required=true,defaultValue="1",value="pageNum")Integer pageNum){
+        //一页有多少条
+        int defaultPageSize=6;
+        //初始化pageHelper对象
+        PageHelper.startPage(pageNum,defaultPageSize);
+        List<UserListVO> userListVOS = applyService.listAllUserInfo();
+        PageInfo<UserListVO> userListVOPageInfo = new PageInfo<>(userListVOS);
+        return userListVOPageInfo;
     }
 
     /*
